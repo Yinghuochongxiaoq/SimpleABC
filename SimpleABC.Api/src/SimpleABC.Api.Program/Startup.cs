34776881d80b752b8f5a6c3488164f8cdcedd8da
FreshMan <qinbocai@sina.cn>
2017-06-16 22:08:07 +0800

    Add BackSystem.

--- SimpleABC.Api/src/SimpleABC.Api.Program/Startup.cs
+++ SimpleABC.Api/src/SimpleABC.Api.Program/Startup.cs
@@ -66,7 +66,12 @@ namespace SimpleABC.Api.Program
             services.AddTransient<IErrorLogBll, ErrorLogBll>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        /// <param name="loggerFactory"></param>
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
